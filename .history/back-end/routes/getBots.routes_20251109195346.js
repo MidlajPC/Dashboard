ae2d2ca08c6a3ddb37c80fb37a28c58cb7cbe718
MidlajPC <midlajpc@drubelabs.com>
2025-11-09 20:57:49 +0530

    line chart

---
+++ .history/back-end/routes/getBots.routes_20251109195346.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const botcontroller = require("../controller/bot.controller");
+const authenticate = require("../middleware/auth.middleware");
+
+router.get("/getbots", authenticate, botcontroller.getbots);
+router.get("/verify",authenticate)
+
+module.exports = router;
